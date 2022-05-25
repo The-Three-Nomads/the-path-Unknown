e84217ab5bdce0b2d4ffdd958a803c9337c9382b
earl-nicholson <earl.r.nicholson@gmail.com>
2022-05-25 12:07:25 -0500

    routing 5/5 complete

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {
+  BrowserRouter as  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+
+
+
+
+
+
 class App extends React.Component {
   render () {
     return (
