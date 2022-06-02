60e16894c258b1b634b882b1127e66a63fa3374d
earl-nicholson <earl.r.nicholson@gmail.com>
2022-06-02 18:01:05 -0500

    fixed heroku css by moving code into scss

--- app/javascript/components/Pages/Locations/LocationNew.js
+++ app/javascript/components/Pages/Locations/LocationNew.js
@@ -43,51 +43,51 @@ class LocationNew extends Component {
         </CardTitle>
         <div className="centeredForm">
         <Form className="formWrapper">
-          <FormGroup className="formGroup">
-            <Label className="label" for="street">Street:</Label>
-            <Input className="formGroup-input" type="text" name="street" onChange={this.handleChange} />
+          <FormGroup className="formGroup1">
+            <Label className="label1" for="street">Street:</Label>
+            <Input className="formGroup-input1" type="text" name="street" onChange={this.handleChange} />
           </FormGroup>
-          <FormGroup className="formGroup">
-            <Label className="label" for="city">City:</Label>
-            <Input className="formGroup-input" type="text" name="city" onChange={this.handleChange} />
+          <FormGroup className="formGroup1">
+            <Label className="label1" for="city">City:</Label>
+            <Input className="formGroup-input1" type="text" name="city" onChange={this.handleChange} />
           </FormGroup>
-          <FormGroup className="formGroup">
-            <Label className="label" for="state">State:</Label>
-            <Input className="formGroup-input" type="text" name="state" onChange={this.handleChange} />
+          <FormGroup className="formGroup1">
+            <Label className="label1" for="state">State:</Label>
+            <Input className="formGroup-input1" type="text" name="state" onChange={this.handleChange} />
           </FormGroup>
-          <FormGroup className="formGroup">
-            <Label className="label" for="description">Description:</Label>
+          <FormGroup className="formGroup1">
+            <Label className="label1" for="description">Description:</Label>
             <Input
-              className="formGroup-input"
+              className="formGroup-input1"
               type="text"
               name="description"
               onChange={this.handleChange}
             />
           </FormGroup>
-          <FormGroup className="formGroup">
-            <Label className="label" for="fee">Fee:</Label>
-            <Input className="formGroup-input" type="text" name="fee" onChange={this.handleChange} />
+          <FormGroup className="formGroup1">
+            <Label className="label1" for="fee">Fee:</Label>
+            <Input className="formGroup-input1" type="text" name="fee" onChange={this.handleChange} />
           </FormGroup>
-          <FormGroup className="formGroup">
-            <Label className="label" for="coordinates">Coordinates:</Label>
+          <FormGroup className="formGroup1">
+            <Label className="label1" for="coordinates">Coordinates:</Label>
             <Input
-              className="formGroup-input"
+              className="formGroup-input1"
               type="text"
               name="coordinates"
               onChange={this.handleChange}
             />
           </FormGroup>
-          <FormGroup className="formGroup">
-            <Label className="label" for="image">Image URL:</Label>
-            <Input className="formGroup-input" type="text" name="image" onChange={this.handleChange} />
+          <FormGroup className="formGroup1">
+            <Label className="label1" for="image">Image URL:</Label>
+            <Input className="formGroup-input1" type="text" name="image" onChange={this.handleChange} />
           </FormGroup>
-          <FormGroup className="formGroup">
-            <Label className="label" for="link">Link to Website:</Label>
-            <Input className="formGroup-input" type="text" name="link" onChange={this.handleChange} />
+          <FormGroup className="formGroup1">
+            <Label className="label1" for="link">Link to Website:</Label>
+            <Input className="formGroup-input1" type="text" name="link" onChange={this.handleChange} />
           </FormGroup>
-          <FormGroup className="formGroup">
-            <Label className="label" for="review">Review:</Label>
-            <Input className="formGroup-input" type="text" name="review" onChange={this.handleChange} />
+          <FormGroup className="formGroup1">
+            <Label className="label1" for="review">Review:</Label>
+            <Input className="formGroup-input1" type="text" name="review" onChange={this.handleChange} />
           </FormGroup>
         <Button className="submitButton" onClick={this.handleSubmit} name="submit">
           Submit
